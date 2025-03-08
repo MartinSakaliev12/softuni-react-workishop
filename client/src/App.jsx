e0820d35a2a1f8a4b823c7522c2fa6f6c5cb4626
Martin Sakaliev <martinsakaliev12@gmail.com>
2025-03-08 20:32:31 +0200

    remove-extra-files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -460,9 +460,7 @@ function App() {
 
   </main>
   {/* <!-- Footer component  --> */}
-  {/* <footer className="footer">
-    <p>This site is designed to be used for training purposes at SoftUni.</p>
-  </footer> */}
+  
 </body>
     </>
   )
